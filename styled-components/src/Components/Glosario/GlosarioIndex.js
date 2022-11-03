323cc74667f390fdb9e879a77581bbe84e571cd5
dalilaorbaj <dalila.orbaj@gmail.com>
2022-11-03 09:44:29 -0300

    First commit :)

---
+++ styled-components/src/Components/Glosario/GlosarioIndex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Glosario = () => {
+    return(
+        <Container>
+          <Button>Normal Button</Button>
+          <Button primary>Primary Button</Button>
+        </Container>
+      );
+}
+
+export default Glosario;
+
